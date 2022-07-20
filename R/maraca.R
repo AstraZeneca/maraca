12ee6ad6e98988259cbcc85155e1e2e59bd85ff6
Stefano Borini <stefano.borini@astrazeneca.com>
2022-07-20 15:56:40 +0100

    Moved private functions and renamed to_rangeab

--- R/maraca.R
+++ R/maraca.R
@@ -1,5 +1,197 @@
 library(ggfortify)
 
+
+
+maraca <- function(filename) {
+
+  `%>%` <- dplyr::`%>%`
+
+  ##########################
+  # Create the HCE dataset #
+  ##########################
+
+  ### Read in the data ###
+  HCE <- read.csv(filename)
+
+  vars <- dplyr::vars
+
+  # Input parameters
+  # Order of the endpoints. The continuous endpoint should always be last.
+  endpoints <- c(
+    "Outcome I", "Outcome II", "Outcome III", "Outcome IV",
+    "Continuous outcome")
+  treatments <- c("Active", "Control")
+  fixed_followup_days <- 3 * 365
+
+  # Remove unwanted endpoints and treatments
+  HCE <- HCE %>%
+    dplyr::filter(GROUP %in% endpoints) %>%
+    dplyr::mutate_at(vars(GROUP), factor, levels = endpoints) %>%
+    dplyr::mutate_at(vars(TRTP), factor, levels = treatments)
+
+
+
+  ##############################################
+  # Function to calculate the Win-Odds results #
+  ##############################################
+  #
+  # wo.result <- calcWinOdds(
+    # data = HCE, ordinalVal = "AVAL", group = "TRTP", reference = "Control")
+
+
+  grp <- sanon::grp
+  fit <- sanon::sanon(AVAL ~ grp(TRTP, ref = "Control"), data = HCE)
+  CI0 <- confint(fit)$ci
+  CI <- CI0 / (1 - CI0)
+  p <- fit$p
+
+  win_odds <- c(CI, p)
+  names(win_odds) <- c("estimate", "lower", "upper", "p-value")
+
+
+
+
+  ##############################################
+  ### Calculations that support the ploting  ###
+  ##############################################
+
+
+  # Calculate meta information from the entire HCE dataset needed for plotting
+
+  meta <- .compute_metainfo(HCE, fixed_followup_days)
+  survmod <- .compute_survmod(HCE, meta, endpoints, treatments)
+  slope <- .compute_slope(HCE, meta, survmod, endpoints, treatments)
+
+  return(
+    structure(
+      list(
+        meta = meta,
+        slope = slope,
+        survmod = survmod,
+        endpoints = endpoints,
+        win_odds = win_odds
+      ),
+      class = c("maraca::maraca")
+    )
+  )
+}
+
+plot_maraca <- function(obj) {
+  aes <- ggplot2::aes
+
+  meta <- obj$meta
+  slope <- obj$slope
+  survmod <- obj$survmod
+  endpoints <- obj$endpoints
+  win_odds <- obj$win_odds
+  start_continuous_endpoint <- meta[meta$GROUP == tail(endpoints, 1), ]$startx
+
+  minor_grid <- seq(
+    sign(min(slope$data$AVAL0)) * floor(abs(min(slope$data$AVAL0)) / 10) * 10,
+    sign(max(slope$data$AVAL0)) * floor(abs(max(slope$data$AVAL0)) / 10) * 10,
+    by = 10
+  )
+
+  zeroposition <- .to_rangeab(0,
+    start_continuous_endpoint,
+    min(slope$data$AVAL0),
+    max(slope$data$AVAL0)
+  )
+  # Plot the information in the Maraca plot
+  plot <- ggplot2::ggplot(survmod$data, aes(colour = TRTP)) +
+    ggplot2::geom_vline(
+      xintercept = cumsum(c(0, meta$proportion)),
+      color = "grey80"
+    ) +
+    ggplot2::geom_vline(
+      xintercept = zeroposition,
+      color = "white",
+      size = 1
+    ) +
+    ggplot2::geom_vline(
+      xintercept = slope$meta$median,
+      color = c("#F8766D", "#00BFC4"), linetype = "dashed", size = 0.3
+    ) +
+    ggplot2::geom_line(
+      data = slope$data,
+      aes(x = violinx, y = violiny, color = TRTP)
+    ) +
+    ggplot2::geom_line(
+      data = survmod$data,
+      aes(x = adjusted.time, y = km.start + km.y * 100, color = strata)
+    ) +
+    ggplot2::geom_violin(
+      data = slope$data,
+      aes(x = x, y = violiny, fill = factor(violiny)), alpha = 0.5
+    ) +
+    ggplot2::geom_boxplot(
+      data = slope$data,
+      aes(x = x, y = violiny, fill = factor(violiny)), alpha = 0.5, width = 2
+    ) +
+    ggplot2::xlab("Type of endpoint") +
+    ggplot2::ylab("Cumulative proportion") +
+    ggplot2::scale_x_continuous(
+      limits = c(0, 100),
+      breaks = c(meta$proportion / 2 + meta$startx),
+      labels = endpoints,
+      minor_breaks = .to_rangeab(
+        minor_grid,
+        start_continuous_endpoint,
+        min(slope$data$AVAL0),
+        max(slope$data$AVAL0)
+      )
+    ) +
+    ggplot2::annotate(
+      geom = "text",
+      x = .to_rangeab(
+        minor_grid,
+        start_continuous_endpoint,
+        min(slope$data$AVAL0),
+        max(slope$data$AVAL0)
+        ),
+      y = 0,
+      label = minor_grid, color = "grey60"
+    ) +
+    ggplot2::annotate(
+      geom = "label",
+      x = 0,
+      y = Inf,
+      label = paste(
+        "Win odds (95% CI): ", round(win_odds[1], 2),
+        " (", round(win_odds[2], 2), ", ", round(win_odds[3], 2), ")", "\n",
+        "p-value: ", format.pval(win_odds[4], digits = 3, eps = 0.001),
+        sep = ""
+      ),
+      hjust = 0, vjust = 1.4, size = 3
+    ) +
+    ggplot2::theme(
+      axis.text.x.bottom = ggplot2::element_text(
+        angle = 90,
+        vjust = 0.5,
+        hjust = 1
+      ),
+      axis.ticks.x.bottom = ggplot2::element_blank(),
+      panel.grid.major.x = ggplot2::element_blank(),
+      axis.title.x.bottom =  ggplot2::element_blank()
+    ) +
+    ggplot2::guides(fill = "none")
+
+  return(plot)
+}
+
+plot_tte_trellis <- function(obj) {
+  survmod <- obj$survmod
+  plot <- ggplot2::ggplot(survmod$data) +
+    ggplot2::geom_line(aes(x = time, y = km.y * 100, color = strata)) +
+    ggplot2::facet_grid(cols = vars(GROUP))
+  return(plot)
+}
+
+`plot.maraca::maraca` <- function(obj) {
+  print(plot_maraca(obj))
+}
+
+# Private
 .win_odds <- function(data, ordinal_val, treatments, reference) {
   grp <- sanon::grp
   fit <- sanon::sanon(
@@ -99,7 +291,7 @@ library(ggfortify)
   ))
 }
 
-to_rangeab <- function(x, start_continuous_endpoint, minval, maxval) {
+.to_rangeab <- function(x, start_continuous_endpoint, minval, maxval) {
     (100 - start_continuous_endpoint) * (x - minval) /
     (maxval - minval) + start_continuous_endpoint
 }
@@ -110,7 +302,7 @@ to_rangeab <- function(x, start_continuous_endpoint, minval, maxval) {
   slope_data <- HCE[HCE$GROUP == tail(endpoints, 1), ]
   start_continuous_endpoint <- meta[meta$GROUP == tail(endpoints, 1), ]$startx
 
-  slope_data$x <- to_rangeab(
+  slope_data$x <- .to_rangeab(
     slope_data$AVAL0,
     start_continuous_endpoint,
     min(slope_data$AVAL0),
@@ -147,194 +339,4 @@ to_rangeab <- function(x, start_continuous_endpoint, minval, maxval) {
 # Function that scale data to a range
 rangeab <- function(x, a, b) {
   (b - a) * (x - min(x)) / (max(x) - min(x)) + a
-}
-
-
-maraca <- function(filename) {
-
-  `%>%` <- dplyr::`%>%`
-
-  ##########################
-  # Create the HCE dataset #
-  ##########################
-
-  ### Read in the data ###
-  HCE <- read.csv(filename)
-
-  vars <- dplyr::vars
-
-  # Input parameters
-  # Order of the endpoints. The continuous endpoint should always be last.
-  endpoints <- c(
-    "Outcome I", "Outcome II", "Outcome III", "Outcome IV",
-    "Continuous outcome")
-  treatments <- c("Active", "Control")
-  fixed_followup_days <- 3 * 365
-
-  # Remove unwanted endpoints and treatments
-  HCE <- HCE %>%
-    dplyr::filter(GROUP %in% endpoints) %>%
-    dplyr::mutate_at(vars(GROUP), factor, levels = endpoints) %>%
-    dplyr::mutate_at(vars(TRTP), factor, levels = treatments)
-
-
-
-  ##############################################
-  # Function to calculate the Win-Odds results #
-  ##############################################
-  #
-  # wo.result <- calcWinOdds(
-    # data = HCE, ordinalVal = "AVAL", group = "TRTP", reference = "Control")
-
-
-  grp <- sanon::grp
-  fit <- sanon::sanon(AVAL ~ grp(TRTP, ref = "Control"), data = HCE)
-  CI0 <- confint(fit)$ci
-  CI <- CI0 / (1 - CI0)
-  p <- fit$p
-
-  win_odds <- c(CI, p)
-  names(win_odds) <- c("estimate", "lower", "upper", "p-value")
-
-
-
-
-  ##############################################
-  ### Calculations that support the ploting  ###
-  ##############################################
-
-
-  # Calculate meta information from the entire HCE dataset needed for plotting
-
-  meta <- .compute_metainfo(HCE, fixed_followup_days)
-  survmod <- .compute_survmod(HCE, meta, endpoints, treatments)
-  slope <- .compute_slope(HCE, meta, survmod, endpoints, treatments)
-
-  return(
-    structure(
-      list(
-        meta = meta,
-        slope = slope,
-        survmod = survmod,
-        endpoints = endpoints,
-        win_odds = win_odds
-      ),
-      class = c("maraca::maraca")
-    )
-  )
-}
-
-plot_maraca <- function(obj) {
-  aes <- ggplot2::aes
-
-  meta <- obj$meta
-  slope <- obj$slope
-  survmod <- obj$survmod
-  endpoints <- obj$endpoints
-  win_odds <- obj$win_odds
-  start_continuous_endpoint <- meta[meta$GROUP == tail(endpoints, 1), ]$startx
-
-  minor_grid <- seq(
-    sign(min(slope$data$AVAL0)) * floor(abs(min(slope$data$AVAL0)) / 10) * 10,
-    sign(max(slope$data$AVAL0)) * floor(abs(max(slope$data$AVAL0)) / 10) * 10,
-    by = 10
-  )
-
-  zeroposition <- to_rangeab(0,
-    start_continuous_endpoint,
-    min(slope$data$AVAL0),
-    max(slope$data$AVAL0)
-  )
-  # Plot the information in the Maraca plot
-  plot <- ggplot2::ggplot(survmod$data, aes(colour = TRTP)) +
-    ggplot2::geom_vline(
-      xintercept = cumsum(c(0, meta$proportion)),
-      color = "grey80"
-    ) +
-    ggplot2::geom_vline(
-      xintercept = zeroposition,
-      color = "white",
-      size = 1
-    ) +
-    ggplot2::geom_vline(
-      xintercept = slope$meta$median,
-      color = c("#F8766D", "#00BFC4"), linetype = "dashed", size = 0.3
-    ) +
-    ggplot2::geom_line(
-      data = slope$data,
-      aes(x = violinx, y = violiny, color = TRTP)
-    ) +
-    ggplot2::geom_line(
-      data = survmod$data,
-      aes(x = adjusted.time, y = km.start + km.y * 100, color = strata)
-    ) +
-    ggplot2::geom_violin(
-      data = slope$data,
-      aes(x = x, y = violiny, fill = factor(violiny)), alpha = 0.5
-    ) +
-    ggplot2::geom_boxplot(
-      data = slope$data,
-      aes(x = x, y = violiny, fill = factor(violiny)), alpha = 0.5, width = 2
-    ) +
-    ggplot2::xlab("Type of endpoint") +
-    ggplot2::ylab("Cumulative proportion") +
-    ggplot2::scale_x_continuous(
-      limits = c(0, 100),
-      breaks = c(meta$proportion / 2 + meta$startx),
-      labels = endpoints,
-      minor_breaks = to_rangeab(
-        minor_grid,
-        start_continuous_endpoint,
-        min(slope$data$AVAL0),
-        max(slope$data$AVAL0)
-      )
-    ) +
-    ggplot2::annotate(
-      geom = "text",
-      x = to_rangeab(
-        minor_grid,
-        start_continuous_endpoint,
-        min(slope$data$AVAL0),
-        max(slope$data$AVAL0)
-        ),
-      y = 0,
-      label = minor_grid, color = "grey60"
-    ) +
-    ggplot2::annotate(
-      geom = "label",
-      x = 0,
-      y = Inf,
-      label = paste(
-        "Win odds (95% CI): ", round(win_odds[1], 2),
-        " (", round(win_odds[2], 2), ", ", round(win_odds[3], 2), ")", "\n",
-        "p-value: ", format.pval(win_odds[4], digits = 3, eps = 0.001),
-        sep = ""
-      ),
-      hjust = 0, vjust = 1.4, size = 3
-    ) +
-    ggplot2::theme(
-      axis.text.x.bottom = ggplot2::element_text(
-        angle = 90,
-        vjust = 0.5,
-        hjust = 1
-      ),
-      axis.ticks.x.bottom = ggplot2::element_blank(),
-      panel.grid.major.x = ggplot2::element_blank(),
-      axis.title.x.bottom =  ggplot2::element_blank()
-    ) +
-    ggplot2::guides(fill = "none")
-
-  return(plot)
-}
-
-plot_tte_trellis <- function(obj) {
-  survmod <- obj$survmod
-  plot <- ggplot2::ggplot(survmod$data) +
-    ggplot2::geom_line(aes(x = time, y = km.y * 100, color = strata)) +
-    ggplot2::facet_grid(cols = vars(GROUP))
-  return(plot)
-}
-
-`plot.maraca::maraca` <- function(obj) {
-  print(plot_maraca(obj))
 }
